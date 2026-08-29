4a86d500eafecfb082229c01cfc13ec3c847b3e8
apervmm <apervmm@gmail.com>
2026-08-28 18:16:47 -0700

    test

--- packages/backend/app/main.py
+++ packages/backend/app/main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.database import connect_db, disconnect_db
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await connect_db()
-    yield
-    await disconnect_db()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 
 
